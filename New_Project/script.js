d6ff21f98578770dd5fcdc56103042c801fb926e
nitesh123pandey <nitesh.pandey1857@gmail.com>
2022-11-25 19:58:55 +0530

    my project code

---
+++ New_Project/script.js
@@ -0,0 +1,4 @@
+function toggleBtn(){
+    var btn = document.getElementById ("btn");
+    var light = document.getElementById ("light")
+}
---
+++ Student_project/scripet.js
@@ -0,0 +1,15 @@
+function Submit(){
+    var student = document.getElementById("student-first-name");
+    var middle = document.getElementById("middle-name");
+    var last = document.getElementById("last-name");
+    var roll = document.getElementById("roll-no");
+    var countryCode = document.getElementById("mobile");
+    var mobileNumber = document.getElementById("6261");
+    var email = document.getElementById("e-mail");
+    var date = document.getElementById("birth");
+    var zen = document.getElementById("gender");
+    var core = document.getElementById("subject");
+    var place = document.getElementById("address");
+
+    console.log(student.value,middle.value,last.value,roll.value,countryCode.value,mobileNumber.value,email.value,date.value,zen.value,core.value,place.value)
+}
